c66ade5e603bd5ace5789b1aceb32f0dd7e0d846
Raven <rrdlpl@gmail.com>
2019-12-08 10:28:40 +0100

    added sky and ground

---
+++ public/js/loaders.js
@@ -0,0 +1,16 @@
+export const loadImage = (url) => {
+    return new Promise((resolve) => {
+        const image = new Image();
+        image.addEventListener('load', () => {
+            resolve(image)
+        })
+        image.src = url;
+    })
+}
+
+export const loadLevel = (name) => {
+    return fetch(`/levels/${name}.json`).then((response) => {
+        return response.json()
+    })
+}
+
